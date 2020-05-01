259667ef4bb8927311717525e3a6cbad13dca877
TigranMnjoyan96 <mnjoyan1996@gmail.com>
2020-05-02 00:09:49 +0400

    smth

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,78 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="grid">
+    <input type="text" v-model="result" placeholder="0">
+    <button @click="input(number)" v-for="(number, key) in numbers" :key="key" class="cell num">{{number}}</button>
+    <button @click="input(opt)" v-for="(opt, key) in operations" :key="key" class="cell op">{{opt}}</button>
+    <button @click="reset" class="cell op">R</button>
+    <button @click="calc()" class="cell op">=</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      result: '',
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+      operations: ['+', '-', '*', '/']
+    }
+  },
+  methods: {
+    input(char) {
+      this.result = this.result.toString()
+        this.result += char;
+    },
+    reset() {
+      this.result = ''
+    },
+    calc() {
+      this.result = eval(this.result)
+    }
   }
+
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+
+input[type="text"] {
+  width: 200px;
+  border: 0;
+  color: #243849;
+  font-size: 18px;
+  padding: 5px 5px;
+  text-align: right;
+  outline: none;
+  border: 1px solid black;
+  padding: 5px 0;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.grid {
+  display: flex;                       
+  flex-wrap: wrap;  
+  justify-content: space-around;
+  width: 200px;
+  margin: 0 auto;
+  
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.cell {
+  flex: 33%;                       
+  height: 50px;
+  font-size: 18px;
+  border: none;
 }
-a {
-  color: #42b983;
+.num {
+  background-color: #243849;
+  color: #fff;
+  font-weight: 400;
 }
+.cell:hover{
+  background-color: #182530;
+}
+.op {
+  background-color: #ff9900;
+  color: #243849;
+}
+
 </style>
